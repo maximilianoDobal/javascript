b4785bb7fe433e33dfcdd4f5b944a8e1d94f6f17
maximilianoDobal <maximilianodobal@gmail.com>
2021-11-14 18:50:08 -0300

    agregado de dark mode con jquery

--- app1.js
+++ app1.js
@@ -1,5 +1,6 @@
 // =======================
 
+// base de productos
 
 class producto {
 
@@ -45,47 +46,41 @@ const filtrado = productos.filter((prod) => prod.marca === "Specialized")
 
 
 
+// ================ USO DE JQUERY =================
 
-// // ===============================
+let variableCambiante = true
 
-
-// class usuario{
-
-//     // CONSTRUCTOR
-//     constructor () {
-
-//         this.usuario = prompt("Ingrese su Usuario")
-//         this.consumidorFinal = prompt("¿Usted es consumidor final?").toLowerCase()
-        
-//         while (this.consumidorFinal !== "si" && this.consumidorFinal !== "no") {
-//             this.consumidorFinal = prompt("Por favor decir si es Consumidor Final").toLowerCase()
-//         }
-        
-//     }
+$("#darkMode-btn").click(() => {
+    $("body").toggleClass("wawa")
+    $(".btn-warning-darkmode").toggleClass("btn-outline-warning")
+    $(".btn-warning-darkmode").toggleClass("btn-warning")
+    $("header").toggleClass("wawa")
+    $(".equipo").toggleClass("wawa")
+    $(".nav-item--dark").toggleClass("navbar__dark")
+    $(".dark__title").toggleClass("dark__title--background")
+    $(".dark__title").toggleClass("bg--grandiant")
+    $("#btn-enviar").toggleClass("btn-warning1")
     
-// }
+    
+    if (variableCambiante === true){
+        $(".logo-dark").attr("src", "./imgs/logo2Dark.png")
+        $(".svg__gradient--1").attr("stop-color", "#1f1d1c")
+        $(".svg__gradient--2").attr("stop-color", "#1f1d1c")
+        $(".equipo").css("background", "linear-gradient(to right, #1f1d1c, #1f1d1c)")
+        $("header").css("box-shadow", "0px 5px 18px #000000")
+        variableCambiante = !variableCambiante
+    } else {
+        $(".logo-dark").attr("src", "./imgs/logo2.png")
+        $(".svg__gradient--1").attr("stop-color", "#ca6a09")
+        $(".svg__gradient--2").attr("stop-color", "#ffe259")
+        $(".equipo").css("background", "linear-gradient(to right, #ca6a09, #ffe259)")
+        variableCambiante = !variableCambiante
+    } 
+    
 
-// const usuario1 = new usuario 
+})
 
-// let iva = 0
-
-// if (usuario1.consumidorFinal === "si"){
-//     iva = 21
-// } else {
-//     iva = 0
-// }
-
-// usuario1.iva = iva
-
-
-// console.log(usuario1)
-// console.log(iva)
-
-
-// ====================================
-
-
-
+// =================================
 const contenedor = document.getElementById("container__productos")
 contenedor.innerHTML = `<h2 class="text-center">Nuestras bicicletas</h2>`
 
@@ -110,11 +105,11 @@ productos.forEach((producto) => {
     productos__hijo.classList.add("productos--hijo", "align-items-center")
     
     productos__hijo.innerHTML = `
-    <h3 class="bg--grandiant px-3">${producto.nombre}</h3>
+    <h3 class="bg--grandiant px-3 dark__title">${producto.nombre}</h3>
     <img src="${producto.img}" alt="" class="img-fluid">
     <p>Lorem ipsum dolor ${producto.marca} sit amet consectetur adipisicing elit. Ex consequatur </p>
     <p id="wawa">Precio: $${producto.precio}</p>
-    <button type="button" class="btn btn-warning" id="agregar${producto.id}">Agregar al carrito</button>    
+    <button type="button" class="btn btn-warning btn-warning-darkmode" id="agregar${producto.id}">Agregar al carrito</button>    
     
     `    
     productos__padre.append(productos__hijo)
@@ -143,16 +138,6 @@ const agregarCarrito = (prodId) => {
     actualizarCarrito()
 }
 
-// funcion para eliminar del carrito 
-const eliminarDelCarrito = (prodId) => {
-    
-    const item = carrito.find((prod) => prod.id === prodId)
-    const indice = carrito.indexOf(item)
-    
-    carrito.splice(indice, 1)
-    
-    actualizarCarrito()
-}
 
 // actualizador de carrito 
 const actualizarCarrito = () => {
@@ -175,13 +160,13 @@ const actualizarCarrito = () => {
         <p class="mb-1">${prod.marca}</p>
         <div class="d-flex w-100 justify-content-between">
         <p>Cantidad: <button id="restarCantidad${prod.id}" class="botonCantidad me-2">-</button><span id="cantidadContador${prod.id}">1</span><button id="sumarCantidad${prod.id}" class="botonCantidad ms-2">+</button></p>
-        <p>Precio unit: ${prod.precio}</p>
+        <p>Precio unit: $${prod.precio}</p>
         </div>
         <p id="subPrecio${prod.id}">Precio: $${prod.precio}</p>
         `
         contenedorCarrito.append(li)
 
-        // boton de eliminar del carrito  ****** Aca cuando toco el boton agregar al carrito varias veces tengo que tocar la misma cantidad el tachito para borrar el item del carrito y no encontre la forma de solucionarlo ******
+        // boton de eliminar del carrito 
         const boton = document.getElementById(`eliminar${prod.id}`)
         boton.addEventListener('click', () => {
             eliminarDelCarrito(prod.id)
@@ -240,4 +225,79 @@ const actualizarCarrito = () => {
     }
     cuentaFinal()
 }
+
+// funcion para eliminar del carrito 
+const eliminarDelCarrito = (prodId) => {
+    
+    const carrito2 = new Set(carrito)
+    const carritoDefinitivo = Array.from(carrito2)
+
+    const item = carritoDefinitivo.find((prod) => prod.id === prodId)
+    const indice = carritoDefinitivo.indexOf(item)
+    do {
+        carrito.splice(indice, 1)
+    } while (carrito.find((prod) => prod.id === prodId));
+    
+    
+    actualizarCarrito()
+}
+
+// =================== Formulario ======================
+
+
+   
+const formulario = document.getElementById('formulario')
+const tilde = document.getElementById('tilde')
+const cruz = document.getElementById('cruz')   
+
+const baseDeMails = []
+
+formulario.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+
+    const mailNews = document.getElementsByClassName('mailNews')[0]
+    const btnEnviar = document.getElementById('btn-enviar')
+
+    
+    if (mailNews.value === ""){
+        cruz.classList.remove('desaparecer')
+        tilde.classList.add('desaparecer')
+        btnEnviar.classList.add('btn-danger')
+        btnEnviar.classList.remove('btn-warning', 'btn-success')
+        
+        
+    } else{
+        tilde.classList.remove('desaparecer')
+        cruz.classList.add('desaparecer')
+        btnEnviar.classList.add('btn-success')
+        btnEnviar.classList.remove('btn-danger', 'btn-warning')
+        btnEnviar.innerText = 'Enviado'
+
+        const usuario = {
+            mail: mailNews.value
+        }
+    
+        baseDeMails.push(usuario)
+        localStorage.setItem('baseDeMails', JSON.stringify(baseDeMails))
+
+        mailNews.value = ""
+
+    }
+    
+    mailNews.addEventListener('input', () => {
+        btnEnviar.classList.remove('btn-danger', 'btn-success')
+        btnEnviar.classList.add('btn-warning')
+        btnEnviar.innerText = 'Enviar'
+        tilde.classList.add('desaparecer')
+        cruz.classList.add('desaparecer')
+    })
+
+    console.log(baseDeMails)
+
+  
+
+
+
+})
 
